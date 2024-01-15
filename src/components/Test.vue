ab56bb4df76e232148bf1f087a6f69d2ebeaeb16
linuxpedro <pdro.dev97@gmail.com>
2024-01-15 07:21:57 -0300

    feat: cards

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,33 +1,89 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="3" md="4">
-                <v-card class="mx-auto">
-    <v-card-title>A maior borboleta do mundo</v-card-title>
+  <v-container>
+    <v-row class="d-flex justify-space-evenly">
+      <v-col cols="12" sm="4">
+        <v-card class="mx-auto">
+          <v-card-title>A maior borboleta do mundo</v-card-title>
 
-    <v-img
-      class="mx-auto align-center text-white"
-      cover
-      width="300"
-      height="300"
-      src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    ></v-img>
+          <v-img
+            class="mx-auto align-center text-white"
+            cover
+            width="300"
+            height="300"
+            src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          ></v-img>
 
-    <v-card-subtitle class="pt-4">Conheça a maior borboleta do mundo</v-card-subtitle>
+          <v-card-subtitle class="pt-4">
+            Conheça a maior borboleta do mundo</v-card-subtitle
+          >
 
-    <v-card-text>
-      <div>Conheça a maior borboleta do mundo</div>
-      <div>Whitsunday Island, Whitsunday Islands</div>
-    </v-card-text>
+          <v-card-text>
+            <div>Conheça a maior borboleta do mundo</div>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
 
-    <v-card-actions>
-      <v-btn color="orange">Visitar</v-btn>
-    </v-card-actions>
-  </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+          <v-card-actions>
+            <v-btn variant="outlined" color="orange">Visitar</v-btn>
+            <v-btn prepend-icon="mdi-calendar-month" variant="tonal" color="orange">Agendar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-card class="mx-auto">
+          <v-card-title>A maior borboleta do mundo</v-card-title>
+
+          <v-img
+            class="mx-auto align-center text-white"
+            cover
+            width="300"
+            height="300"
+            src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          ></v-img>
+
+          <v-card-subtitle class="pt-4">
+            Conheça a maior borboleta do mundo</v-card-subtitle
+          >
+
+          <v-card-text>
+            <div>Conheça a maior borboleta do mundo</div>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn variant="outlined" color="orange">Visitar</v-btn>
+            <v-btn prepend-icon="mdi-calendar-month" variant="tonal" color="orange">Agendar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-card class="mx-auto">
+          <v-card-title>A maior borboleta do mundo</v-card-title>
+
+          <v-img
+            class="mx-auto align-center text-white"
+            cover
+            width="300"
+            height="300"
+            src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          ></v-img>
+
+          <v-card-subtitle class="pt-4">
+            Conheça a maior borboleta do mundo</v-card-subtitle
+          >
+
+          <v-card-text>
+            <div>Conheça a maior borboleta do mundo</div>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn variant="outlined" color="orange">Visitar</v-btn>
+            <v-btn prepend-icon="mdi-calendar-month" variant="tonal" color="orange">Agendar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
