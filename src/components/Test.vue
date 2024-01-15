0e90bef406cc9dfd5a1e2e136862dbef137e0a11
linuxpedro <pdro.dev97@gmail.com>
2024-01-15 09:10:11 -0300

    feat: new cards

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
-    <v-row class="d-flex justify-space-evenly">
-      <v-col cols="12" sm="4">
+    <v-row>
+      <v-col cols="12" sm="6" md="4" lg="3">
         <v-card class="mx-auto">
           <v-card-title>A maior borboleta do mundo</v-card-title>
 
@@ -24,11 +24,16 @@
 
           <v-card-actions>
             <v-btn variant="outlined" color="orange">Visitar</v-btn>
-            <v-btn prepend-icon="mdi-calendar-month" variant="tonal" color="orange">Agendar</v-btn>
+            <v-btn
+              prepend-icon="mdi-calendar-month"
+              variant="tonal"
+              color="orange"
+              >Agendar</v-btn
+            >
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="12" sm="4">
+      <v-col cols="12" sm="6" md="4" lg="3">
         <v-card class="mx-auto">
           <v-card-title>A maior borboleta do mundo</v-card-title>
 
@@ -51,11 +56,16 @@
 
           <v-card-actions>
             <v-btn variant="outlined" color="orange">Visitar</v-btn>
-            <v-btn prepend-icon="mdi-calendar-month" variant="tonal" color="orange">Agendar</v-btn>
+            <v-btn
+              prepend-icon="mdi-calendar-month"
+              variant="tonal"
+              color="orange"
+              >Agendar</v-btn
+            >
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="12" sm="4">
+      <v-col cols="12" sm="6" md="4" lg="3">
         <v-card class="mx-auto">
           <v-card-title>A maior borboleta do mundo</v-card-title>
 
@@ -78,7 +88,140 @@
 
           <v-card-actions>
             <v-btn variant="outlined" color="orange">Visitar</v-btn>
-            <v-btn prepend-icon="mdi-calendar-month" variant="tonal" color="orange">Agendar</v-btn>
+            <v-btn
+              prepend-icon="mdi-calendar-month"
+              variant="tonal"
+              color="orange"
+              >Agendar</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <v-card class="mx-auto">
+          <v-card-title>A maior borboleta do mundo</v-card-title>
+
+          <v-img
+            class="mx-auto align-center text-white"
+            cover
+            width="300"
+            height="300"
+            src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          ></v-img>
+
+          <v-card-subtitle class="pt-4">
+            Conheça a maior borboleta do mundo</v-card-subtitle
+          >
+
+          <v-card-text>
+            <div>Conheça a maior borboleta do mundo</div>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn variant="outlined" color="orange">Visitar</v-btn>
+            <v-btn
+              prepend-icon="mdi-calendar-month"
+              variant="tonal"
+              color="orange"
+              >Agendar</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <v-card class="mx-auto">
+          <v-card-title>A maior borboleta do mundo</v-card-title>
+
+          <v-img
+            class="mx-auto align-center text-white"
+            cover
+            width="300"
+            height="300"
+            src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          ></v-img>
+
+          <v-card-subtitle class="pt-4">
+            Conheça a maior borboleta do mundo</v-card-subtitle
+          >
+
+          <v-card-text>
+            <div>Conheça a maior borboleta do mundo</div>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn variant="outlined" color="orange">Visitar</v-btn>
+            <v-btn
+              prepend-icon="mdi-calendar-month"
+              variant="tonal"
+              color="orange"
+              >Agendar</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <v-card class="mx-auto">
+          <v-card-title>A maior borboleta do mundo</v-card-title>
+
+          <v-img
+            class="mx-auto align-center text-white"
+            cover
+            width="300"
+            height="300"
+            src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          ></v-img>
+
+          <v-card-subtitle class="pt-4">
+            Conheça a maior borboleta do mundo</v-card-subtitle
+          >
+
+          <v-card-text>
+            <div>Conheça a maior borboleta do mundo</div>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn variant="outlined" color="orange">Visitar</v-btn>
+            <v-btn
+              prepend-icon="mdi-calendar-month"
+              variant="tonal"
+              color="orange"
+              >Agendar</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <v-card class="mx-auto">
+          <v-card-title>A maior borboleta do mundo</v-card-title>
+
+          <v-img
+            class="mx-auto align-center text-white"
+            cover
+            width="300"
+            height="300"
+            src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          ></v-img>
+
+          <v-card-subtitle class="pt-4">
+            Conheça a maior borboleta do mundo</v-card-subtitle
+          >
+
+          <v-card-text>
+            <div>Conheça a maior borboleta do mundo</div>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn variant="outlined" color="orange">Visitar</v-btn>
+            <v-btn
+              prepend-icon="mdi-calendar-month"
+              variant="tonal"
+              color="orange"
+              >Agendar</v-btn
+            >
           </v-card-actions>
         </v-card>
       </v-col>
